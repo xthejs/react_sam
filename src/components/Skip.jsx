8b823ad6479f335e53889a0c35fe65a7ea88a2b6
xthejs <xthejs@gmail.com>
2024-07-29 12:10:53 +0900

    1._header완성
    1._header완성

---
+++ src/components/Skip.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Skip = () => {
+    return (
+        <div id='skip'>
+            <a href="#header">헤더영역 바로가기</a>
+            <a href="#intro">인트로영역 바로가기</a>
+            <a href="#skill">스킬영역 바로가기</a>
+            <a href="#site">사이트영역 바로가기</a>
+            <a href="#port">포트폴리오영역 바로가기</a>
+            <a href="#contact">연락처영역 바로가기</a>
+            <a href="#footer">푸터영역 바로가기</a>
+        </div>
+    )
+}
+
+export default Skip
